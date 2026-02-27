24772a2d03c77fed8551b7066b7d38ce73aace4c
Harshitt <hbatham910@gmail.com>
2026-02-27 22:47:51 +0530

    Update App.jsx

--- smartlearn-ai/client/src/App.jsx
+++ smartlearn-ai/client/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
